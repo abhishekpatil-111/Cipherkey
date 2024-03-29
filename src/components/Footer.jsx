46c9a403da6888b8bb798e3412263e26a7abc8fb
abhishek patil <abhishekp01234@gmail.com>
2024-03-29 20:41:32 +0530

    made some changes in the footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -10,32 +10,34 @@ const Footer = () => {
                 <span className="text-green-700">key /</span>
                 <span className="text-green-700">&gt;</span>
             </div>
-            <div className='flex justify-center items-center'>
-                <lord-icon
+            <div className='flex justify-center items-center w-full'>
+                {/* <lord-icon
                     src="https://cdn.lordicon.com/nvrrlies.json"
                     trigger="loop"
                     delay="1000"
                     state="morph-code"
                     colors="primary:#121331,secondary:#3a3347,tertiary:#ebe6ef,quaternary:#16c72e,quinary:#ffc738,senary:#f24c00">
-                </lord-icon>
-                Created With <lord-icon
+                </lord-icon> */}
+                Created with 
+                <lord-icon
                     src="https://cdn.lordicon.com/igciyimj.json"
                     trigger="loop"
                     delay="2000"
                     stroke="light"
                     state="morph-glitter"
                     colors="primary:#121331,secondary:#c71f16,tertiary:#ffc738,quaternary:#f9c9c0,quinary:#f24c00,senary:#ebe6ef">
-                </lord-icon> By &nbsp;
+                </lord-icon> 
+                By &nbsp;
                 <a href='https://abhishek-patil-official.netlify.app/'>
                 <span className='font-bold'>Abhishek Patil</span>
                 </a>
                 <a title='follow on Linkedin' href="https://www.linkedin.com/in/abhishek-patil-a46621229/">
-                    <img className='ml-2 mb-2' src="/icons/linkedin.png" alt="" style={{ width: "30px", height: "30px", paddingTop: "3px", }} />
+                    <img className='ml-1 mb-2' src="/icons/linkedin.png" alt="" style={{ width: "30px", height: "30px", paddingTop: "3px", }} />
                 </a>
 
-                <a title='follow on instagram' href='https://www.instagram.com/abhie_patil_7917?igsh=MXg2MjdsOHdsam96OQ=='>
+                {/* <a title='follow on instagram' href='https://www.instagram.com/abhie_patil_7917?igsh=MXg2MjdsOHdsam96OQ=='>
                     <img className='ml-2 mb-2' src="/icons/instagram.png" alt="" style={{ width: "30px", height: "31px", paddingTop: "3px", }} />
-                </a>
+                </a> */}
 
             </div>
         </div>
